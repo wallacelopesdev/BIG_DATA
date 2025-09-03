7c88ccf5a9bda8500b9b2d9d97e4548e62d75f8a
WALLACE SILVA LOPES <202503078998@alunos.unifanor.edu.br>
2025-09-03 18:57:50 -0300

    atualizandoR

---
+++ Big Data1/varieveis/dadosUsuario.py
@@ -0,0 +1,7 @@
+nome = str(input("Digite seu nome: "))
+
+idade = int(input("Digite sua idade: "))
+
+dadosUsuario = f"Seu nome é {nome} e você tem {idade} anos."
+
+print(dadosUsuario)
---
+++ Big Data1/varieveis/exercicios.py
@@ -0,0 +1,14 @@
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+
+adiçao = n1 + n2
+divisao = n1 / n2
+multiplicaçao = n1 * n2
+subtraçao = n1 - n2
+restodadivisao = n1 % n2
+
+print(f"A soma entre {n1} e {n2} é igual a {adiçao}")
+print(f"A divisao entre {n1} e {n2} é igual a {divisao}")
+print(f"A multiplicaçao entre {n1} e {n2} é igual a {multiplicaçao}")
+print(f"A subtraçao entre {n1} e {n2} é igual a {subtraçao}")
+print(f"O resto da divisao entre {n1} e {n2} é igual a {restodadivisao}")
